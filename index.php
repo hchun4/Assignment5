feeccc9460ed4e66bf3d487be07f29ab79a12e4a
rosecricchio <rcricchi@u.rochester.edu>
2017-10-05 17:13:24 -0400

    update

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
         		<div class="col s12 m4">
           			<div class="icon-block">
           				<h3 class="center">Hillary Chun</h3>
-          				<a href="hillarychun.php"><img src="images/hillary.jpg" class="images" alt="Hillary Chun"></a>
           				<a href="hillarychun.php"><img src="images/hillary.jpg" class="images z-depth-2" alt="Hillary Chun"></a>
 						<p class="light">Hillary is a Public Health and Psychology double major and is UR class of '18. She is from Bronx, NY and enjoys photography, cooking, and music.</p>
 						
@@ -21,7 +20,6 @@
         		<div class="col s12 m4">
           			<div class="icon-block">
           				<h3 class="center rose">Rose Cricchio</h3>
-          				<a href="rosecricchio.php"><img src="images/rose.jpg" class="images" alt="Rose Cricchio"></a>
           				<a href="rosecricchio.php"><img src="images/rose.jpg" class="images z-depth-2" alt="Rose Cricchio"></a>
 						<p class="light">Rose is UR class of '19 and is double majoring in Economics and Computer Science. She is from Anchorage, AK and enjoys dog-spotting, winter and napping.</p>
 					</div>
@@ -30,7 +28,6 @@
         		<div class="col s12 m4">
           			<div class="icon-block">
           				<h3 class="center">Tori Madway</h3>
-          				<a href="torimadway.php"><img src="images/tori.jpg" class="images" alt="Tori Madway"></a>
           				<a href="torimadway.php"><img src="images/tori.jpg" class="images z-depth-2" alt="Tori Madway"></a>
 						<p class="light">Tori is currently attending UR after previously attending Vassar College. At UR, she is considering a DMS major and Math minor. She enjoys acrobalancing, homebrewing, and creating things.</p>
 					</div>
